2815ccece28e5f0226a47656cd0676c0be407da9
aruna-vr <aruna@gogosoon.com>
2023-05-29 01:12:22 +0530

    [Aruna] pull user info from AWS cognito

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,65 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from './page.module.css'
+"use client";
 
-const inter = Inter({ subsets: ['latin'] })
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "./page.module.css";
+import { useEffect, useState } from "react";
+import { useSearchParams } from "next/navigation";
+import axios from "axios";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const searchParams = useSearchParams();
+  const code = searchParams.get("code");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  useEffect(() => {
+    if (!code) return;
+    const clientID = process.env.NEXT_PUBLIC_COGINTO_CLIENT_ID || "";
+    const clientSecret = process.env.NEXT_PUBLIC_COGNITO_CLIENT_SECRET || "";
+    const cognitoDomain = process.env.NEXT_PUBLIC_COGNITO_DOMAIN || "";
+    const credentials = `${clientID}:${clientSecret}`;
+    const base64Credentials = Buffer.from(credentials).toString("base64");
+    const basicAuthorization = `Basic ${base64Credentials}`;
+    const headers = {
+      "Content-Type": "application/x-www-form-urlencoded",
+      Authorization: basicAuthorization,
+    };
+    const data = new URLSearchParams();
+    let token = "";
+    data.append("grant_type", "authorization_code");
+    data.append("client_id", clientID);
+    data.append("code", code);
+    data.append("redirect_uri", "http://localhost:3000");
+    axios
+      .post(
+        `${cognitoDomain}/oauth2/token`,
+        data,
+        { headers }
+      )
+      .then((res) => {
+        if (res.status != 200) return;
+        token = res?.data?.access_token;
+        const userInfoHeaders = {
+          Authorization: "Bearer " + token,
+        };
+        axios
+          .get(
+            `${cognitoDomain}/oauth2/userInfo`,
+            { headers: userInfoHeaders }
+          )
+          .then((userInfo) => {
+            if (userInfo.status != 200) return;
+            setName(userInfo.data?.username);
+            setEmail(userInfo.data?.email);
+          });
+      });
+  }, [code]);
+
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-      </div>
+      <div className={styles.description}></div>
 
       <div className={styles.center}>
         <Image
@@ -18,13 +69,17 @@ export default function Home() {
           height={200}
         />
       </div>
-      <h2 className={inter.className}>
-           Welcome to 5minslearn!
-          </h2>
+      <h2 className={inter.className}>Welcome to 5minslearn!</h2>
+      {name && email ? (
+        <>
+          <h2 className={inter.className}>{name}</h2>
+          <p className={inter.className}>{email}</p>
+        </>
+      ) : (
+        <></>
+      )}
 
       <div className={styles.center}>
-        
-
         <a
           href="https://5minslearn.gogosoon.com/?ref=github_nextjs_app"
           className={styles.card}
@@ -36,9 +91,7 @@ export default function Home() {
           </h2>
           <p className={inter.className}>Learn tech in 5mins</p>
         </a>
-
-        
       </div>
     </main>
-  )
+  );
 }
